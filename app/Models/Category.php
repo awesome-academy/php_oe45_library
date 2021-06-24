81b2725678fc6e1d37232a1b685716da6dd6008a
MinhTung2504 <minhtungg2504@gmail.com>
2021-06-24 23:53:01 +0700

    Init models, add relationship

---
+++ app/Models/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Book;
+
+class Category extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $primaryKey = 'cate_id';
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'cate_name',
+        'cate_desc',
+        'parent_id',
+    ];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class, 'cate_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+    
+    public function parent()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,16 +6,27 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Authorfollow;
+use App\Models\Bookfollow;
+use App\Models\Borrow;
+use App\Models\Like;
+use App\Models\Review;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory; 
+    use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+    protected $primaryKey = 'user_id';
+    protected $dates = ['deleted_at'];
+
     protected $fillable = [
         'name',
         'email',
@@ -40,4 +51,29 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function authorfollows() 
+    {
+        return $this->hasMany(Authorfollow::class, 'user_id');
+    }
+
+    public function bookfollows() 
+    {
+        return $this->hasMany(Bookfollow::class, 'book_id');
+    }
+
+    public function borrows() 
+    {
+        return $this->hasMany(Borrow::class, 'user_id');
+    }
+
+    public function likes() 
+    {
+        return $this->hasMany(Like::class, 'user_id');
+    }
+
+    public function reviews() 
+    {
+        return $this->hasMany(Review::class, 'user_id');
+    }
 }
